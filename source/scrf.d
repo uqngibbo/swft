50c641d791f6b6f83600164cdfea4c6d8405a286
Nick N. Gibbons <n.gibbons@uq.edu.au>
2025-08-20 11:24:07 +1000

    srcf: WIP commit for conserrved version of the formulation

--- source/scrf.d
+++ source/scrf.d
@@ -54,11 +54,11 @@ double temp_from_u(GasState gs, GasModel gm){
     return gs.u.re/cv;
 }
 
-Primitives increment_primitives(double x, double A, double dx, double dA, double Hdot, double f, ref Primitives U0, ref GasModel gm, ref GasState gs){
-    double rho = U0.rho;
-    double p = U0.p;
-    double v = U0.v;
-    double u = U0.u;
+Primitives increment_primitives(double x, double A, double dx, double dA, double Hdot, double f, ref Primitives P0, ref GasModel gm, ref GasState gs){
+    double rho = P0.rho;
+    double p = P0.p;
+    double v = P0.v;
+    double u = P0.u;
 
     double du_chem = 0.0;
     double dp_chem = 0.0;
@@ -94,6 +94,131 @@ Primitives increment_primitives(double x, double A, double dx, double dA, double
                       v   = v   + dv,
                       u   = u   + du_gda);
 }
+
+double[3] increment_conserved(double x, double A, double dx, double dA, double Hdot, double f, ref Primitives P0, ref GasModel gm, ref GasState gs){
+
+    double dAdx = dA/dx;
+    double Ast = dAdx/(A/dx + dAdx); // FIXME: Note that the A here should be A1...
+
+    double Fmass1 = P0.rho*P0.v;
+    double Fmom1 = P0.rho*P0.v*P0.v + P0.p;
+    double Fnrg1 = P0.v*(P0.rho*(P0.u + 0.5*P0.v*P0.v) + P0.p);
+
+    double Fmass1_Ast = Fmass1*Ast;
+    //double Fmom1_Ast = Fmom1*Ast;
+    //double Fmom1_Ast = Fmom1*Ast - 0.5*P0.p*dAdx;
+    //double Fmom1_Ast = (Fmom1 - 0.5*P0.p)*Ast;
+    double Fmom1_Ast = (Fmom1 - P0.p)*Ast;
+    double Fnrg1_Ast = Fnrg1*Ast;
+
+    // These increments calculated using reformulate_jac.max. See 18/08 derivation.
+    double gamma = gm.gamma(gs).re;
+    double u = P0.u;
+    double v = P0.v;
+
+    // Double check the signs here, on the RHS specifically
+    double dmass = -(2*Fmass1_Ast*u*gamma^^2-Fmass1_Ast*v^^2*gamma
+                                       +2*Fmom1_Ast*v*gamma
+                                       -2*Fmass1_Ast*u*gamma-2*Fnrg1_Ast*gamma
+                                       -3*Fmass1_Ast*v^^2+2*Fnrg1_Ast)
+                     /(2*v*(u*gamma^^2-u*gamma-v^^2));
+ 
+    double dmom = -Fmass1_Ast; // FR?
+
+    double dnrg = -(2*Fmass1_Ast*u*v*gamma^^2+Fmass1_Ast*v^^3*gamma
+                                        -2*Fmom1_Ast*v^^2*gamma
+                                        -6*Fmass1_Ast*u*v*gamma
+                                        +2*Fnrg1_Ast*v*gamma
+                                        +4*Fmom1_Ast*u*gamma-3*Fmass1_Ast*v^^3
+                                        +4*Fmom1_Ast*v^^2-6*Fnrg1_Ast*v)
+            /(4*(u*gamma^^2-u*gamma-v^^2));
+
+
+    double mass0 = P0.rho;
+    double mom0  = P0.rho*P0.v;
+    double nrg0  = P0.rho*(P0.u + 0.5*P0.v*P0.v);
+	writefln("dmass: %e dmom: %e dnrg: %e ", dmass, dmom, dnrg);
+
+    double[3] U1 = [mass0 + dmass, mom0+dmom, nrg0 + dnrg];
+    return U1;
+}
+
+// We need to check the original matrix by taking the increments from the
+// primitive version and multiply them by J. We should get the right had side.
+
+Primitives decode_conserved(double[3] U, ref GasModel gm, ref GasState gs){
+    double rho = U[0];
+    double v = U[1]/rho;
+    double u = U[2]/rho - 0.5*v*v;
+    gs.u = u;
+    gs.rho = rho;
+    gm.update_thermo_from_rhou(gs);
+
+    double T = gs.T.re;
+    double p = gs.p.re;
+    return Primitives(rho = rho, p = p, v = v, u = u);
+}
+
+void check_flux(Primitives P0, Primitives P1, double A0, double dA, double dx, double gamma){
+
+    double A1 = A0 + dA;
+    double dAdx = dA/dx;
+
+    double mass0 = P0.rho;
+    double mom0  = P0.rho*P0.v;
+    double nrg0  = P0.rho*(P0.u + 0.5*P0.v*P0.v);
+
+    double mass1 = P1.rho;
+    double mom1  = P1.rho*P1.v;
+    double nrg1  = P1.rho*(P1.u + 0.5*P1.v*P1.v);
+
+    double Fmass0 = P0.rho*P0.v;
+    double Fmom0 = P0.rho*P0.v*P0.v + P0.p;
+    double Fnrg0 = P0.v*(P0.rho*(P0.u + 0.5*P0.v*P0.v) + P0.p);
+
+    double Fmass1 = P1.rho*P1.v;
+    double Fmom1 = P1.rho*P1.v*P1.v + P1.p;
+    double Fnrg1 = P1.v*(P1.rho*(P1.u + 0.5*P1.v*P1.v) + P1.p);
+
+
+    double dFAdx_mass = (A1*Fmass1 - A0*Fmass0)/dx;
+    double dFAdx_mom = (A1*Fmom1 - A0*Fmom0)/dx - P0.p*dAdx;
+    double dFAdx_nrg = (A1*Fnrg1 - A0*Fnrg0)/dx;
+    writefln("dFAdx_mass = %e",  dFAdx_mass);
+    writefln("dFAdx_mom  = %e",  dFAdx_mom );
+    writefln("dFAdx_nrg  =  %e", dFAdx_nrg );
+
+    double v = P0.v;
+    double u = P0.u;
+    double[3] dFdU0 = [0,1,0];
+    double[3] dFdU1 = [(v^^2*(gamma-3))/2,-v*(gamma-3),gamma-1];
+	double[3] dFdU2 = [(v*(v^^2*gamma-2*u*gamma-2*v^^2))/2,-(2*v^^2*gamma-2*u*gamma-3*v^^2)/2,v*gamma];
+
+    double[3] dU = [mass1-mass0, mom1-mom0, nrg1-nrg0];
+
+    double Ast = dAdx/(A1/dx + dAdx);
+    double[3] rhs = [-Fmass0*Ast,
+                     //-Fmom0*Ast,
+                     //-Fmom0*Ast + 0.5*P0.p*dAdx,
+                     //-(Fmom0-0.5*P0.p)*Ast,
+                     -(Fmom0-P0.p)*Ast,
+                     -Fnrg0*Ast];
+
+    double[3] lhs = [dFdU0[0]*dU[0] + dFdU0[1]*dU[1] + dFdU0[2]*dU[2],
+                     dFdU1[0]*dU[0] + dFdU1[1]*dU[1] + dFdU1[2]*dU[2],
+                     dFdU2[0]*dU[0] + dFdU2[1]*dU[1] + dFdU2[2]*dU[2]];
+
+    writefln("lhs-> %15.15e, %15.15e, %15.15e", lhs[0], lhs[1], lhs[2]);
+    writefln("rhs-> %15.15e, %15.15e, %15.15e", rhs[0], rhs[1], rhs[2]);
+    writefln("diff> %15.15f, %15.15f, %15.15f\n",(lhs[0]-rhs[0])/rhs[0]*100.0,
+                                                 (lhs[1]-rhs[1])/rhs[1]*100.0,
+                                                 (lhs[2]-rhs[2])/rhs[2]*100.0);
+
+}
+
+
+
+
 
 int main(string[] args)
 {
@@ -136,8 +261,11 @@ int main(string[] args)
 
     double gamma = gm.gamma(gs).re;
 
-    Primitives U0 = Primitives(gs.rho.re, gs.p.re, v, gs.u.re);
-    simdata ~= SimData(U0.p, gs.T.re, U0.rho, As, U0.v, M, gamma);
+    Primitives P0 = Primitives(gs.rho.re, gs.p.re, v, gs.u.re);
+    double[3] U0 = [P0.rho,
+                    P0.rho*v,
+                    P0.rho*(P0.u + 0.5*v*v)];
+    simdata ~= SimData(P0.p, gs.T.re, P0.rho, As, P0.v, M, gamma);
     xs ~= x;
 
     size_t iter = 0; 
@@ -157,21 +285,25 @@ int main(string[] args)
         double A1 = PI*r1*r1;
         double dA = A1-A;
 
-        Primitives U1 = increment_primitives(x, A, dx, dA, Hdot, f, U0, gm, gs);
+        Primitives P1p = increment_primitives(x, A, dx, dA, Hdot, f, P0, gm, gs);
+        //double[3] U1  = increment_conserved( x, A, dx, dA, Hdot, f, P0, gm, gs);
+        //Primitives P1c =  decode_conserved(U1, gm, gs);
+        Primitives P1 = P1p;
 
         // Add the increments
         x = x + dx;
-        gs.rho = U1.rho;
-        gs.u = U1.u;
-        gs.p = U1.p;
+        gs.rho = P1.rho;
+        gs.u = P1.u;
+        gs.p = P1.p;
         gs.T = temp_from_u(gs, gm);
+        //gs.T = gs.T.re;
         gm.update_sound_speed(gs);
         gamma = gm.gamma(gs).re;
 
-        M = U1.v/gs.a.re;
-        simdata ~= SimData(U1.p, gs.T.re, U1.rho, A, U1.v, M, gamma);
+        M = P1.v/gs.a.re;
+        simdata ~= SimData(P1.p, gs.T.re, P1.rho, A, P1.v, M, gamma);
         xs ~= x;
-        U0 = U1;
+        P0 = P1;
 
         iter += 1;
         if ((iter%20==0)||(last_step)){
