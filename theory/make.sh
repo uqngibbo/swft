02274174e78ddfe73b85d8a0bd81ef0c6f0bd7dd
Nick N. Gibbons <n.gibbons@uq.edu.au>
2025-09-17 17:10:23 +1000

    Switched to automatically computed heat derivatives

--- theory/make.sh
+++ theory/make.sh
@@ -1,5 +1,10 @@
 
-maxima --batch nonuniform_friction_derivs.max | grep -v '(%' | tail -n +9 > nonuniform_friction_derivs.d
-sed -i 's/\^/\^\^/g' nonuniform_friction_derivs.d
-sed -i 's/ ;/;/g' nonuniform_friction_derivs.d
-sed -i 's/[[:space:]]\+$//' nonuniform_friction_derivs.d
+maxima --batch nonuniform_friction_derivs.max | grep -v '(%' | tail -n +9 > derivs_friction.d
+sed -i 's/\^/\^\^/g' derivs_friction.d
+sed -i 's/ ;/;/g' derivs_friction.d
+sed -i 's/[[:space:]]\+$//' derivs_friction.d
+
+maxima --batch heat_addition_derivative.max | grep -v '(%' | tail -n +9 > derivs_heat.d
+sed -i 's/\^/\^\^/g' derivs_heat.d
+sed -i 's/ ;/;/g'  derivs_heat.d
+sed -i 's/[[:space:]]\+$//' derivs_heat.d
